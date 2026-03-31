9ed2730ddd83c30dd6b1c31444142d2119000286
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-03-31 14:22:54 +0900

    HtJWT start, AuthTokenService

---
+++ src/main/java/com/back/domain/member/service/AuthTokenService.java
@@ -0,0 +1,7 @@
+package com.back.domain.member.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthTokenService {
+}
---
+++ src/test/java/com/back/domain/member/service/AuthTokenServiceTest.java
@@ -0,0 +1,23 @@
+package com.back.domain.member.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class AuthTokenServiceTest {
+
+    @Autowired
+    private AuthTokenService authTokenService;
+
+    @Test
+    void t1() {
+        assertThat(authTokenService).isNotNull();
+    }
+}
