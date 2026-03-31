f18d22a38156c296989a5b14d3e943cb1dcc0e42
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-03-31 14:50:50 +0900

    JWT 키 테스트로 만들어보기

--- src/test/java/com/back/domain/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/back/domain/member/service/AuthTokenServiceTest.java
@@ -1,10 +1,18 @@
 package com.back.domain.member.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,5 +27,30 @@ public class AuthTokenServiceTest {
     @Test
     void t1() {
         assertThat(authTokenService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("jjwt 최신 방식으로 JWT 생성, {name=\"Paul\", age=26}")
+    void t2() {
+        // 토큰 만료기간: 1년
+        long expireMillis = 1000L * 60 * 60 * 24 * 365;
+
+        byte[] keyBytes = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes(StandardCharsets.UTF_8);
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBytes);
+
+        // 발행 시간과 만료 시간 설정
+        Date issuedAt = new Date();
+        Date expiration = new Date(issuedAt.getTime() + expireMillis);
+
+        String jwt = Jwts.builder()
+                .claims(Map.of("name", "Paul", "age", 26)) // 내용
+                .issuedAt(issuedAt) // 생성날짜
+                .expiration(expiration) // 만료날짜
+                .signWith(secretKey) // 키 서명
+                .compact();
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt = " + jwt);
     }
 }
