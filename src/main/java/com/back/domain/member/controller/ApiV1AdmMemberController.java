dc0bef3aa7dfb87be794c176c42ea3b7f715fe39
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-04-02 12:33:43 +0900

    Spring Security

--- src/main/java/com/back/domain/member/controller/ApiV1AdmMemberController.java
+++ src/main/java/com/back/domain/member/controller/ApiV1AdmMemberController.java
@@ -36,4 +36,6 @@ public class ApiV1AdmMemberController {
                 .toList();
 
     }
+
+
 }
---
+++ src/main/java/com/back/global/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.back.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers("/favicon.ico").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated())
+                .csrf((csrf) -> csrf.disable())
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+
+        return http.build();
+    }
+}
