b4d16dd14751a0f3d5ad3464d79530c7f29108fb
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-04-02 12:11:07 +0900

    만료된 íê´post admin

--- src/main/java/com/back/domain/member/controller/ApiV1AdmMemberController.java
+++ src/main/java/com/back/domain/member/controller/ApiV1AdmMemberController.java
@@ -1,7 +1,10 @@
 package com.back.domain.member.controller;
 
 import com.back.domain.member.dto.MemberWithUsernameDto;
+import com.back.domain.member.entity.Member;
 import com.back.domain.member.service.MemberService;
+import com.back.global.exception.ServiceException;
+import com.back.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +18,17 @@ import java.util.List;
 public class ApiV1AdmMemberController {
 
     private final MemberService memberService;
+    private final Rq rq;
 
     @GetMapping
     public List<MemberWithUsernameDto> list() {
+
+        Member actor = rq.getActor(); // 인증
+
+        // 관리자 권한 체크
+        if(!actor.isAdmin()){
+            throw new ServiceException("403-1", "권한이 없습니다.");
+        }
 
         return memberService.findAll()
                 .stream()
--- src/test/java/com/back/domain/member/controller/ApiV1AdmMemberControllerTest.java
+++ src/test/java/com/back/domain/member/controller/ApiV1AdmMemberControllerTest.java
@@ -1,7 +1,7 @@
 package com.back.domain.member.controller;
 
+import com.back.domain.member.entity.Member;
 import com.back.domain.member.repository.MemberRepository;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +10,13 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.containsInRelativeOrder;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -40,7 +42,7 @@ public class ApiV1AdmMemberControllerTest {
 
         resultActions
                 .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
-                .andExpect(handler().methodName("getItems"))
+                .andExpect(handler().methodName("list"))
                 .andExpect(status().isOk());
 
         resultActions
@@ -66,5 +68,28 @@ public class ApiV1AdmMemberControllerTest {
 //                    .andExpect(jsonPath("$[%d].title".formatted(i)).value(post.getTitle()))
 //                    .andExpect(jsonPath("$[%d].content".formatted(i)).value(post.getContent()));
 //        }
+    }
+
+    @Test
+    @DisplayName("회원 다건 조회, 권한이 없는 경우")
+    void t2() throws Exception {
+
+        Member actor = memberRepository.findByUsername("user1").get();
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/adm/members")
+                                .header(
+                                        "Authorization", "Bearer %s".formatted(actor.getApiKey())
+                                )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
+                .andExpect(handler().methodName("list"))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.resultCode").value("403-1"))
+                .andExpect(jsonPath("$.msg").value("권한이 없습니다."));
     }
 }
